677b432e047b677897c96be9fc7cb276288ca668
GongT <admin@gongt.me>
2019-01-07 13:37:34 +0800

    add global 7za link

---
+++ 7za.js
@@ -0,0 +1,8 @@
+const path7za = require('7zip-bin').path7za.replace(/\.asar([\/\\])/, (m0, sp) => {
+	return '.asar.unpacked' + sp;
+});
+
+require('child_process').spawnSync(path7za, process.argv.slice(2), {
+	stdio: 'inherit',
+	shell: false,
+});
